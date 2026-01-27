9365b09e6519c827b4e324b7fb58ff45aa56a7af
Nikhil <nikhilagrawal6448@gmail.com>
2026-01-27 21:54:55 +0530

    initilizaed repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hero from "./pages/hero"
+
+function App() {
+
+  return (
+    <div>
+      <Hero />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/hero.tsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+    return (
+        <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/background.webp')" }}>
+        </div>
+    )
+}
+
+export default Hero
