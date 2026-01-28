114cb680ee923287640f2e542f671d88638e1e20
Paramveer <68002445+paramveer-cyber@users.noreply.github.com>
2026-01-28 15:26:30 +0530

    minor structure and design changes

--- src/pages/Navbar/navbar.tsx
+++ src/pages/Navbar/navbar.tsx
@@ -10,23 +10,11 @@ const Navbar = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <div ref={ref} className="navbar">
       <img className="branch" src={branch} alt="branch" />
-
       <div className="lanterns">
-        <a href="/about">
-          <img className="lantern l1" src={lanterne} />
-        </a>
-
-        <a href="/events">
-          <img className="lantern l2" src={lanterna} />
-        </a>
-
-        <a href="/speakers">
-          <img className="lantern l3" src={lanterns1} />
-        </a>
-
-        <a href="/sponsors">
-          <img className="lantern l4" src={lanterns2} />
-        </a>
+        <a href="/about"><img className="lantern l1"    alt="lantern 1" src={lanterne} /></a>
+        <a href="/events"><img className="lantern l2"   alt="lantern 2" src={lanterna} /></a>
+        <a href="/speakers"><img className="lantern l3" alt="lantern 3" src={lanterns1} /></a>
+        <a href="/sponsors"><img className="lantern l4" alt="lantern 4" src={lanterns2} /></a>
       </div>
     </div>
   );
