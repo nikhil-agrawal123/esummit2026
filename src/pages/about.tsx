aa02742577ba2e78b27c1fa2ad0e07d5222aa6e5
Navneet Ajmani <navneetajmani17@gmail.com>
2026-02-22 18:45:16 +0530

    about page updated

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,67 @@
-import randomImg from "../assets/flower.webp"
+import React from "react";
+import bg from "../assets/about/background.png";
+import building from "../assets/about/IIITD_asset.png";
+import logo from "../assets/about/logo.png";
 
-function About() {
+const About: React.FC = () => {
   return (
-    <>
-        <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/background1.png')" }}>
-            <img className="top-0 left-0" src={randomImg} alt="" />
-            <div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, officiis.</div>
-        </div>
-    </>
-  )
-}
+    <section
+      className="w-full h-screen relative overflow-hidden"
+      style={{
+        backgroundImage: `url(${bg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+<div className="absolute left-[12%] top-[55%] -translate-y-1/2 z-20 flex items-start gap-[60px]">
 
-export default About
+  {/* Vertical Line */}
+  <div className="w-[3.5px] h-[500px] bg-[#7b3f00]" />
+
+  {/* Text Block */}
+  <div className="max-w-[540px] flex flex-col gap-[38px]">
+
+    <h1
+      className="uppercase text-[100px] leading-[1.1] tracking-[0.05em] text-[#371B2E]"
+      style={{ fontFamily: "Akumaru" }}
+    >
+      ABOUT THE <br /> FESTIVAL
+    </h1>
+
+    <div
+      className="text-[25px] leading-[40px] text-[#5a4a42] max-w-[500px]"
+      style={{ fontFamily: "Calibri, sans-serif" }}
+    >
+      <p>
+        PitchCafe Competitive Platforms For Students And Startups
+        To Pitch Ideas To Investors For Funding.
+        PitchCafe Competitive Platforms For Students And Startups
+        To Pitch Ideas To Investors For Funding.
+      </p>
+    </div>
+
+  </div>
+</div>
+
+{/* TOP LEFT LOGO */}
+<img
+  src={logo}
+  alt="E-Summit logo"
+  className="absolute top-[40px] left-[110px] w-[200px] z-30 object-contain select-none"
+  draggable={false}
+/>
+
+      {/* BUILDING – MID LAYER */}
+        <img
+        src={building}
+        alt="building"
+        className="absolute right-0 bottom-[0%] w-[55%] max-w-none z-0 object-contain select-none"
+        draggable={false}
+        />
+    </section>
+  );
+};
+
+
+
+export default About;
