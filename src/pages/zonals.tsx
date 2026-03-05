dd58b4160e1004b8e0dd9c645262afdbb3bd1832
Nikhil Agrawal <nikhilagrawal6448@gmail.com>
2026-03-05 23:12:13 +0530

    zonal mobile view done

--- src/pages/zonals.tsx
+++ src/pages/zonals.tsx
@@ -1,31 +1,57 @@
 import React from "react";
 import bg from "../assets/zonals_background.webp";
-import timeline from "../assets/zonals_timeline.webp"; 
+import timeline from "../assets/zonals_timeline.webp";
 
 const Zonals: React.FC = () => {
   return (
     <section
       id="zonals"
       className="w-full min-h-screen relative overflow-hidden -mt-60"
-      style={{
-        backgroundImage: `url(${bg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
     >
-      <img
-        src={timeline}
-        alt="zonals timeline"
-        className="absolute left-[18%] top-1/2 -translate-y-1/2 w-[33%] max-w-none z-10 object-contain select-none"
-        draggable={false}
-      />
-
-      <h1
-        className="absolute right-[15%] top-[50%] -translate-y-1/2 text-[110px] text-[#371B2E] uppercase z-20"
-        style={{ fontFamily: "Akumaru" }}
+      {/* Desktop View */}
+      <div
+        className="hidden md:block w-full h-full min-h-screen relative"
+        style={{
+          backgroundImage: `url(${bg})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
-        ZONALS
-      </h1>
+        <img
+          src={timeline}
+          alt="zonals timeline"
+          className="absolute left-[18%] top-1/2 -translate-y-1/2 w-[33%] max-w-none z-10 object-contain select-none"
+          draggable={false}
+        />
+
+        <h1
+          className="absolute right-[15%] top-[50%] -translate-y-1/2 text-[110px] text-[#371B2E] uppercase z-20"
+          style={{ fontFamily: "Akumaru" }}
+        >
+          ZONALS
+        </h1>
+      </div>
+
+      {/* Mobile View */}
+      <div className="md:hidden w-full min-h-screen relative flex flex-col pt-8 pl-4"
+          style={{
+            backgroundImage: `url(${bg})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}>
+        <h1
+          className="text-[48px] text-[#371B2E] uppercase mb-4 "
+          style={{ fontFamily: "Akumaru" }}
+        >
+          ZONALS
+        </h1>
+        <img
+          src="/zonalmob.png"
+          alt="zonals mobile"
+          className="w-[80%] h-auto object-contain select-none"
+          draggable={false}
+        />
+      </div>
     </section>
   );
 };
